29a55166ed34b976f6aa8c567643f18438cdf74e
bikashnepali9743-max <bikashnepali9743@gmail.com>
2026-05-25 12:05:01 +0900

    Add files via upload
    first push

---
+++ intro.py
@@ -0,0 +1,69 @@
+
+
+"""
+a=10
+b=10
+c=a+b
+
+"""
+
+#print(c)
+#int,float,bool,str
+
+
+#variable
+
+if 5>6:
+    print("it is true")
+else:
+    print("it is false")
+    #Print"I am"and the number 25
+    #This is a comment
+    print("hello World")
+    # Write a single-line comment
+    #　Comment out this  line so it dosenot run
+    print("This should not run")
+    # This is a comment
+    # Comment out this line so it doesnot run:
+    #print (" this should not run")
+    """This is
+      a multiline 
+      comment 
+    """
+    print("hello kcg")
+    x=10
+    x="Sally"#x is now of type str
+    print(x)
+    x=str(3) # x will be "3"
+    y=int(3) # y will be 3
+    z=int(3) # z will be 3.0
+    print(x)
+    print(y)
+    print("hello bikash")
+    print("I am single")#yes
+    fruits={"apple","banana","cherry",}
+    x,y,z=fruits
+    print(x)
+    print(z)
+    print(y)
+    x="Bikash"
+    y="Hari"
+    y="Ram"
+    print(x,y,z)
+    x=5
+    y="Jhon"
+    print(x,y)
+    x="awesome"
+    a="hello"
+    print(a)
+    a="Hello,World"
+    print(a.upper())
+    a="hello,Nepal!"
+    print(a.lower())
+    b="Hello,World!"
+    print=(b[2:5])
+    bool("abc")
+    bool(123)
+    bool(["apple","cherry","banana"])
+    thislist={"apple","banana","cherry"}
+    print(thislist)
